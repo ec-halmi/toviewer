ad60abf71ab62721369a4ec7199491059eedfac0
halmi yasin <halmi@ilovekl.net>
2025-11-14 19:22:02 +0800

    update config

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,50 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 
-export default defineConfig({
+export default defineConfig( {
   server: {
     port: 3322,
   },
-  build: {
+  base: '/viewer/', // This base URL will apply to both dev and build
+
+  optimizeDeps: { // <-- Moved to top level
+    include: [ '@thatopen/components', 'three', 'web-ifc' ],
+  },
+
+  build: { // This section is ONLY used when running 'npm run build'
+    minify: false,
+    // sourcemap: true, 
+    outDir: 'dist', // Default directory for build output
     rollupOptions: {
-      input: 'index.html', // Ensure this matches the location of your index.html
+      input: 'index.html',
       output: {
-        dir: 'dist',
         entryFileNames: 'assets/main.js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name].[ext]',
+        manualChunks: ( id ) =>
+        {
+          if ( id.includes( '@thatopen/components' ) )
+          {
+            return 'components';
+          }
+          if ( id.includes( 'fileLoader' ) )
+          {
+            return 'fileLoader';
+          }
+          if ( id.includes( 'three' ) )
+          {
+            return 'three';
+          }
+          if ( id.includes( 'web-ifc' ) )
+          {
+            return 'webifc';
+          }
+          if ( id.includes( 'node_modules' ) )
+          {
+            return 'vendor';
+          }
+        },
       },
     },
-    optimizeDeps: {
-      include: ["@thatopen/components", "three", "web-ifc"],
-    },
   },
-});
+} );
